40211968198d40a75f289c7ca6a3f207a1c01f49
Henrique Oliveira <holiveira_93@hotmail.com>
2025-09-20 19:30:30 -0300

    04.05 - Testando erro 400

---
+++ src/test/java/med/voll/api/controllers/ConsultaControllerTest.java
@@ -0,0 +1,33 @@
+package med.voll.api.controllers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class ConsultaControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Deveria devolver código http 400 quando informacoes estao invalidas")
+    @WithMockUser
+    void agendarCenario1() throws Exception {
+        var response = mockMvc.perform(post("/consultas"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+}
